890f4a1e6e5ab2a24e997d8777a8505304048b33
Zoe Ferencova <zoeferencova@gmail.com>
2019-12-23 20:06:46 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+require('dotenv').config()
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const MOVIES = require('./MOVIES.json');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(helmet());
+
+function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    next();
+}
+
+app.use(validateBearerToken);
+
+function handleGetMovies(req, res) {
+    let response = MOVIES;
+
+    const { genre, country, avg_vote } = req.query;
+
+    if (genre) {
+        response = response.filter(movie => movie.genre.toLowerCase().includes(genre.toLowerCase()))
+    }
+
+    if (country) {
+        response = response.filter(movie => movie.country.toLowerCase().includes(country.toLowerCase()))
+    }
+
+    const avg_vote_number = Number(avg_vote);
+
+    if (avg_vote) {
+        if (Number.isNaN(avg_vote_number)) {
+            res.status(400).json({ error: 'avg_vote should be a number' })
+        } else {
+            response = response.filter(movie => Number(movie.avg_vote) >= avg_vote_number)
+        }
+    }
+    
+    res.send(response)
+}
+
+
+
+app.get('/movie', handleGetMovies)
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`);
+})
